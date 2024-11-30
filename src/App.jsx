5095e8fa75d1502205490ef336801ffa7990e796
fatima <sfateme262@gmail.com>
2024-11-30 20:31:26 +0330

    add change componnet app

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,6 @@ function App() {
          <Route path="/" element={<Home />}/>
          <Route path= "/login"  element={<Login/>}/> 
          <Route path='/carBody' element={<CarBody/>} />
-      
    </Routes>
    </>
   )
