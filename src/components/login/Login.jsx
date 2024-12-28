b0e8d9276fd92b65d55ec2dadd6fbd7cbe60beec
rezix <wonderword2002@gmail.com>
2024-12-28 10:48:58 +0330

    login

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -97,7 +97,7 @@ export default function Login() {
       const { timeOut, isActive } = JSON.parse(storedData);
       setData({ timeOut });
       setIsActive(isActive);
-      setShowLink(timeOut === 0); // این خط جدید است
+      setShowLink(timeOut === 0); 
     }
   }, []);
   
@@ -145,15 +145,26 @@ export default function Login() {
   const verifyCode = async () => {
     try {
       const response = await axiosInstance.post('user/verify-code', {
-        code: code.code, 
+        phone: phone.phone,
+        code: code.code,
       });
+      const { token } = response.data;
+  
+      if (token) {
+        Cookies.set('X-token', token, { expires: 7 });
+        console.log('Token:', token);
+      } else {
+        console.error('Token not found in response');
+      }
+  
       console.log('Code verified successfully:', response.data);
+  
       return response.data;
     } catch (error) {
       console.error('Error verifying code:', error.response?.data || error.message);
-      // throw error;
     }
   };
+  
 
   const validate = () => {
     const { error } = schema.validate(phone, { abortEarly: false });
@@ -168,6 +179,7 @@ export default function Login() {
       return false;
     }
     Cookies.set("phone", phone.phone, { expires: 7 });
+   
     return true;
   };
 
@@ -300,7 +312,10 @@ export default function Login() {
                     <div className="w-[3px] rounded-2xl h-5 bg-[#55c7e0]"></div>
                     <p
                       className="text-[14px] cursor-pointer max-sm:text-[12px]"
-                      onClick={generateNewCode}
+                      onClick={() => {if (validate()) {
+                        sendSmsCode(phone.phone);
+                        navigate("/login/password");
+                      }}}
                     >
                       ارسال دوباره کد
                     </p>
@@ -322,3 +337,86 @@ export default function Login() {
 
   );
 }
+
+
+
+
+
+
+
+
+
+
+
+// import { Routes, Route } from 'react-router-dom';
+// import './App.css'
+// import Home from './components/Home.jsx';
+// import Login from './components/login/Login.jsx';
+// import CarBody from './components/introduction/CarBody.jsx'
+// import Life from "./components/introduction/Life.jsx"
+// import Treatment from "./components/introduction/Treatment.jsx"
+// import Pet from "./components/introduction/Pet.jsx"
+// import Mobile from "./components/introduction/Mobile.jsx"
+// import Psychology from "./components/introduction/Psychology.jsx"
+// import LegalAdvice from "./components/introduction/LegalAdvice.jsx"
+// import Input from './components/input/Input.jsx';
+// import CreateUser from "./components/dashbord/CreateUser.jsx"
+// import Registration from "./components/dashbord/Registration.jsx"
+// import Contract from "./components/dashbord/Contract.jsx"
+// import Ministrant from "./components/introduction/Ministrant.jsx"
+// import MyContracts from './components/managementPanel/MyContracts.jsx';
+// import Profile from './components/managementPanel/Profile.jsx';
+// import Services from './components/managementPanel/ServicesPanel.jsx';
+// import Support from './components/managementPanel/Support.jsx';
+// import GetDashbordUsers from './components/dashbord/GetDashbordUsers.jsx'
+// import ManagementPanel from "./components/managementPanel/ManagementPanel.jsx"
+// import ChildProfil from "./components/managementPanel/ChildProfil.jsx"
+// import Test from "./components/test/Test.jsx"
+
+
+// function App() {
+ 
+//    return (
+   
+//    <>    
+//     <Routes>
+//          <Route path="/"      element={<Home />}/>
+//          <Route path='/carBody'  element={<CarBody/>} />
+//          <Route path='/life'     element={<Life/>} />
+//          <Route path='/treatment'  element={<Treatment/>} />
+//          <Route path='/pet'   element={<Pet/>} />
+//          <Route path='/mobile'  element={<Mobile/>} />
+//          <Route path='/psychology' element={<Psychology/>} />
+//          <Route path='/legal-advice' element={<LegalAdvice/>} />
+//          <Route path='/ministrant' element={<Ministrant/>} />
+//          <Route path="login/*"  element={<Login/>}>
+//                 <Route
+//                  index element={ <Input />} />
+//                 <Route
+//                   path="password"
+//                   element={<Input /> } />
+//          </Route>
+
+//          <Route path="dashbord/*"  element={<ManagementPanel/>}>
+//             <Route index   element={< Profile/>}/>
+//             <Route path='profile' element={< Profile/>}/>
+//             <Route path='table' element={< ChildProfil/>}/>
+        
+//             <Route path="mycontracts"  element={<MyContracts />}/>
+//             <Route path="services"  element={<Services />}/>
+//            <Route  path="support" element={<Support />}/>
+//          </Route>
+
+//          <Route path='/user-create' element={<CreateUser/>} />
+//         <Route path='/insurance' element={<Registration/>} />
+//         <Route path='/user'  element={<GetDashbordUsers/>} />
+//         <Route path='/contract' element={<Contract/>} />
+//         <Route path='/test'  element={<Test/>} />
+
+//    </Routes>
+  
+//    </>
+//   )
+// }
+
+// export default App
