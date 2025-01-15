7c30afec46fe016f7b5bd5f403553cc5ac04cd9c
safari <sfateme262@gmail.com>
2025-01-15 09:25:21 +0330

    remove additional text in panel

--- src/components/managementPanel/ManagementPanel.jsx
+++ src/components/managementPanel/ManagementPanel.jsx
@@ -68,13 +68,13 @@ export default function ManagementPanel() {
       <div className="w-full flex h-screen  relative  ">
         <div className={` h-16  absolute  flex lg:flex-row-reverse items-center ${!toggle?'w-full':'w-9/12'}`}>
           <img src={toggle ? icon.close : icon.menu} className={`${!toggle ? "w-10 h-8 p-[2px] mr-4" : "w-10 h-6 ml-2 order-2"}  font-[#4f4f4f]  cursor-pointer z-50 lg:hidden `} onClick={handleMenu}></img>
-          <div className="lg:w-10/12 max-lg:w-full h-full flex items-center justify-end px-4  ">
+          <div className="lg:w-10/12 max-lg:w-full h-full flex items-center justify-end px-4 " >
             {/* <img src="../img/pannel/Notification.svg" alt="" className="md:w-10 md:h-10 lg:mr-20 md:mr-12 mr-2 max-md:w-7 max-md:h-7" /> */}
             <img src="../img/icon/ASAF Logo copy.svg" alt="" className="md:h-14 max-md:h-10 xl:pl-10 xl:mt-10" />
           </div>
         </div>
 
-        <div className=" h-full w-3/12 bg-white rounded-e-xl z-10 shadow-2xl flex flex-col gap-20 items-center pt-28 max-lg:hidden ">
+        <div className=" h-full w-3/12 bg-white rounded-e-xl z-10 shadow-2xl flex flex-col gap-20 items-center pt-28 max-lg:hidden" >
           <div className=" w-[120px] h-[120px]  rounded-full relative">
             <img src="../img/pannel/User.svg" alt="" className="object-center " />
             
@@ -92,8 +92,8 @@ export default function ManagementPanel() {
                   isActive ? 'bg-[rgba(33,48,99,0.9)] rounded-xl h-10 flex items-center text-white ' : 'text-[#4e4e4e] hover:bg-[rgba(250,250,250,0.9)] rounded-xl '
                 }
               >
-                <li className="flex items-center gap-4  xl:text-[18px]  h-10  ' : ''
-                }]">
+                <li className="flex items-center gap-4  xl:text-[18px]  h-10 
+                ">
                   <span className="w-3 h-3 bg-[#55c7e0] rounded-full mr-2  block "></span>
                   پروفایل
                 </li>
